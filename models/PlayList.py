e330350cf0e1013c7c470fffaeb64edf1399eab5
kaestro <didme654@snu.ac.kr>
2024-05-14 05:34:23 +0900

    PlayList implemented

--- models/PlayList.py
+++ models/PlayList.py
@@ -1,6 +1,5 @@
 # models/PlayList.py
 from pydantic import BaseModel
-
 from models.Market import Market
 
 
@@ -9,9 +8,30 @@ class Playlist(BaseModel):
 
 
 class SpotifyPlayList(Playlist):
+    def __init__(
+        self,
+        playlist_id: str,
+        market: Market,
+        fields: list[str],
+        additional_types: list[str],
+    ):
+        self._playlist_id = playlist_id
+        self._market = market
+        self._fields = fields
+        self._additional_types = additional_types
 
-    def __init__(self):
-        playlist_id: str
-        market: Market
-        fields: list[str]
-        additional_types: list[str]
+    @property
+    def playlist_id(self):
+        return self._playlist_id
+
+    @property
+    def market(self):
+        return self._market
+
+    @property
+    def fields(self):
+        return self._fields
+
+    @property
+    def additional_types(self):
+        return self._additional_types
